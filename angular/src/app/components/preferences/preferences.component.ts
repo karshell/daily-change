e50a0fa8e4808bbb9ff2446e0aed3a676ac0a6e9
Kevin Shelley <kevin.shelley@unitron.com>
2019-01-31 20:02:43 -0500

    preferences form validation

--- angular/src/app/components/preferences/preferences.component.ts
+++ angular/src/app/components/preferences/preferences.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AppContextService } from '../../services/app-context.service';
 import { Preferences } from './preferences';
 import { Router } from '@angular/router';
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-preferences',
@@ -15,14 +16,50 @@ export class PreferencesComponent {
     private goalCurrency: string;
     private goalTargetDate: Date;
 
-    constructor(private appContextService: AppContextService, private router: Router) { }
+    private showTitleError: boolean;
+    private showTargetError: boolean;
+    private showCurrencyError: boolean;
+    private showDateError: boolean;
+
+    constructor(private appContextService: AppContextService, private router: Router) {
+        this.showTitleError = false;
+        this.showTargetError = false;
+        this.showDateError = false;
+        this.showCurrencyError = false;
+     }
 
     currencies = ['CAD', 'USD', 'EUR'];
 
     onSubmit() {
+        this.showTitleError = false;
+        this.showTargetError = false;
+        this.showCurrencyError = false;
+        this.showDateError = false;
+
+        console.log(this.goalTitle);
+        if (!this.goalTitle) {
+            this.showTitleError = true;
+            return;
+        }
+
+        if (!this.goalAmount || this.goalAmount < 1) {
+            this.showTargetError = true;
+            return;
+        }
+
+        if (!this.goalCurrency) {
+            this.showCurrencyError = true;
+            return;
+        }
+
+        if (!this.goalTargetDate) {
+            this.showDateError = true;
+            return;
+        }
+
         const preferences = new Preferences(this.goalTitle, this.goalAmount, this.goalTargetDate, this.goalCurrency);
         this.appContextService.SetPreferences(preferences);
-        this.router.navigate(['indexasd']);
+        this.router.navigate(['index']);
     }
 
     getMinTargetDate(): Date {
